bd34736dc5a13db261b1aa3862caef8af8bef587
yamnor <yamnor@gmail.com>
2024-07-12 16:27:31 +0900

    Updates

---
+++ precache-manifest.e6bd459427e2ae143ab774b13b57c40c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afdf85973cdd9f446af1c738b99a26f4",
+    "url": "/thesis-checklist-tool/index.html"
+  },
+  {
+    "revision": "90a6f3839231bdd9a479",
+    "url": "/thesis-checklist-tool/static/css/main.633f19dd.chunk.css"
+  },
+  {
+    "revision": "c0ee305eb3145ecf5a2b",
+    "url": "/thesis-checklist-tool/static/js/2.07b41d29.chunk.js"
+  },
+  {
+    "revision": "90a6f3839231bdd9a479",
+    "url": "/thesis-checklist-tool/static/js/main.9f9d0eb4.chunk.js"
+  },
+  {
+    "revision": "cc103d3f32bec52e585a",
+    "url": "/thesis-checklist-tool/static/js/runtime~main.3baa8eda.js"
+  }
+]);
